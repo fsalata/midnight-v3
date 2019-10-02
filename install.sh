644b4d002a3bd2ff136e73304b30d0c784d897f3
Fábio Salata <fabio.salata@oreons.com>
2019-10-02 16:08:47 -0300

    Added themes and install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv Midnight-v3.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
+mv Midnight-v3-FC.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
